e7b08df878aedb614973276abbea0abf8f80b2d7
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-06-02 00:56:32 +0630

    Update start_server.ps1

--- start_server.ps1
+++ start_server.ps1
@@ -1,10 +1,10 @@
 $ProgressPreference = 'SilentlyContinue'
 wget -O "C:\\nomachine.exe" is.gd/nomachine
-Start-Process "C:\\nomachine.exe" -Arguments "/Silent" -PassThru
+Start-Process "C:\\nomachine.exe" -Argument "/Silent" -PassThru
 wget -O "C:\\ngrok.zip" is.gd/ngrokdl
 Expand-Archive "C:\\ngrok.zip" -DestinationPath "C:\\ngrok"
 wget -O "C:\\qb.exe" is.gd/qbset
-Start-Process "C:\\qb.exe" -Arguments "/Silent" -PassThru
+Start-Process "C:\\qb.exe" -Argument "/Silent" -PassThru
 C:\\ngrok\ngrok.exe authtoken 2Q9No8jfW3i3lWyMQsWrN9GvPJD_3sP5i7GnhSqjnWdxiAsii
 wget -O "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ngrok.bat" https://kiyoe.hka.workers.dev/0:/ngrok.bat
 $PASSWORD= ConvertTo-SecureString -AsPlainText -Force -String Kiyoe4Life!
