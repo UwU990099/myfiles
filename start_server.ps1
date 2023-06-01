ac220305da6266a9175e75b1e86af28608747853
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-06-01 23:00:42 +0630

    Update start_server.ps1

--- start_server.ps1
+++ start_server.ps1
@@ -1,1 +1,12 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) && choco install nomachine -y && choco install ngrok -y && choco install qbittorrent -y && ngrok authtoken 2Q9No8jfW3i3lWyMQsWrN9GvPJD_3sP5i7GnhSqjnWdxiAsii && wget -O "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ngrok.bat" https://kiyoe.hka.workers.dev/0:/ngrok.bat && $PASSWORD= ConvertTo-SecureString -AsPlainText -Force -String Kiyoe4Life! && New-LocalUser -Name "Henry" -Password $PASSWORD && Add-LocalGroupMember -Group "Administrators" -Member "Henry" && Restart-Computer
+$ProgressPreference = 'SilentlyContinue'
+wget -O "C:\\nomachine.exe" is.gd/nomachine
+-StartProcess "C:\\nomachine.exe" -Arguments "/Silent" -PassThru
+wget -O "C:\\ngrok.zip" is.gd/ngrokdl
+Expand-Archive "C:\\ngrok.zip" -DestinationPath "C:\\ngrok"
+wget -O "C:\\qb.exe" is.gd/qbset
+-StartProcess "C:\\qb.exe" -Arguments "/Silent" -PassThru
+C:\\ngrok\ngrok.exe authtoken 2Q9No8jfW3i3lWyMQsWrN9GvPJD_3sP5i7GnhSqjnWdxiAsii
+wget -O "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ngrok.bat" https://kiyoe.hka.workers.dev/0:/ngrok.bat
+$PASSWORD= ConvertTo-SecureString -AsPlainText -Force -String Kiyoe4Life!
+New-LocalUser -Name "Henry" -Password $PASSWORD
+Add-LocalGroupMember -Group "Administrators" -Member "Henry"
