3b9d33d7e1c50c34d8d2662d9e98abf34d4199dd
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-06-16 01:12:04 +0630

    Update start_server.ps1

--- start_server.ps1
+++ start_server.ps1
@@ -1,3 +1,15 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install nomachine -y
+choco install ngrok -y
+choco install qbittorrent -y
+ngrok authtoken 2Q9No8jfW3i3lWyMQsWrN9GvPJD_3sP5i7GnhSqjnWdxiAsii
+wget -O "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ngrok.bat" https://kiyoe.hka.workers.dev/0:/ngrok.bat
+$PASSWORD= ConvertTo-SecureString -AsPlainText -Force -String Kiyoe4Life!
+New-LocalUser -Name "Henry" -Password $PASSWORD
+Add-LocalGroupMember -Group "Administrators" -Member "Henry"
+Restart-Computer
+
+// manual
 $ProgressPreference = 'SilentlyContinue'
 wget -O "C:\\nomachine.exe" is.gd/nomachine
 Start-Process "C:\\nomachine.exe" -Argument "/Silent" -PassThru
