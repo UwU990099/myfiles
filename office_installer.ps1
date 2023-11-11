bb5bd0d9c989b5e2ab13ef5028fb5274c8c3c916
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-12 01:59:48 +0630

    Update office_installer.ps1

--- office_installer.ps1
+++ office_installer.ps1
@@ -2,5 +2,5 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "https://officecdn.microsoft.com/pr/wsus/setup.exe" -OutFile "C:\setup.exe"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UwU990099/myfiles/main/Configuration.xml" -OutFile "C:\Configuration.xml"
 Set-Location -Path 'C:\'
-Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'Configuration.xml'
-exit
+Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'Configuration.xml' -wait
+irm https://massgrave.dev/get | iex
