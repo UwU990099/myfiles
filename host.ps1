4d257210c8ca83141da6fbff5b3e1f2c47524f90
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-12 00:42:31 +0630

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -1,27 +1,3 @@
-# Specify the path to the registry setting for desktop wallpaper
-$wallpaperRegistryPath = "HKCU\Software\Policies\Microsoft\Windows\Personalization"
-
-# Specify the name for the "desktop wallpaper" policy
-$wallpaperValueName = "Wallpaper"
-
-# Remove the desktop wallpaper policy by setting it to $null
-Set-GPRegistryValue -Key $wallpaperRegistryPath -ValueName $wallpaperValueName -Type String -Value $null
-
-# Specify the path to the registry setting for hiding the notification area
-$notificationAreaRegistryPath = "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
-
-# Specify the name for the "hide the notification area" policy
-$notificationAreaValueName = "NoTrayItemsDisplay"
-
-# Set the "hide the notification area" policy to 0 to disable it
-Set-GPRegistryValue -Key $notificationAreaRegistryPath -ValueName $notificationAreaValueName -Type DWORD -Value 0
-
-# Stop the Windows Explorer process
-Stop-Process -Name explorer -Force
-
-# Start the Windows Explorer process
-Start-Process explorer
-
 # Download Ngrok
 Invoke-WebRequest -Uri "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip" -OutFile "C:\ngrok.zip"
 
@@ -39,3 +15,6 @@ net user Administrator HenryRH9!
 
 # Run ngrok with specified parameters
 Start-Process -FilePath "C:\ngrok\ngrok.exe" -ArgumentList "tcp", "--region", "ap", "3389" -Wait
+
+# Close window
+exit
