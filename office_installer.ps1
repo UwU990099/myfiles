1c458e447250624fe1388a8730d2d9099ba42e6c
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-13 00:06:06 +0630

    Update office_installer.ps1

--- office_installer.ps1
+++ office_installer.ps1
@@ -1,6 +1,22 @@
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri "https://officecdn.microsoft.com/pr/wsus/setup.exe" -OutFile "C:\setup.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UwU990099/myfiles/main/Configuration.xml" -OutFile "C:\Configuration.xml"
-Set-Location -Path 'C:\'
-Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'Configuration.xml' -wait
-irm https://massgrave.dev/get | iex
+$downloadUrl = "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x64&language=en-us&version=O16GA"
+$savePath = [System.IO.Path]::Combine([System.IO.Path]::GetFolderPath([System.Environment+SpecialFolder]::MyDocuments), "Downloads\OfficeSetup.exe")
+
+# Create a WebClient object
+$webClient = New-Object System.Net.WebClient
+
+# Download the file
+$webClient.DownloadFile($downloadUrl, $savePath)
+
+# Start the setup and wait for it to complete
+$setupProcess = Start-Process -FilePath $savePath -PassThru
+$setupProcess.WaitForExit()
+
+# Run the additional command after setup is complete
+if ($setupProcess.ExitCode -eq 0) {
+    Write-Host "Setup completed successfully. Running additional command..."
+    Invoke-Expression (Invoke-RestMethod -Uri "https://massgrave.dev/get")
+} else {
+    Write-Host "Setup encountered an error. Additional command not executed."
+}
+
+Write-Host "Script complete."
