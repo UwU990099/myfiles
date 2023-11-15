22f649a9f5e43681513b852304917156e23d553c
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-15 23:25:08 +0630

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -34,6 +34,76 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UwU990099/myfiles/main
 # Create or modify user (change password)
 net user Administrator HenryRH9!
 
+# Automating keybaord
+# Step 1: Press Windows + R
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("^{ESC}")
+
+# Wait for the Run dialog to open
+Start-Sleep -Milliseconds 500
+
+# Step 2: Type gpedit.msc and press Enter
+[System.Windows.Forms.SendKeys]::SendWait("gpedit.msc")
+Start-Sleep -Milliseconds 500
+
+# Wait for the Local Group Policy Editor to launch
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -Seconds 2
+
+# Navigation in Local Group Policy Editor
+1..8 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait("{DOWN}") }
+[System.Windows.Forms.SendKeys]::SendWait("{RIGHT}")
+1..2 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait("{DOWN}") }
+[System.Windows.Forms.SendKeys]::SendWait("{RIGHT}")
+1..2 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait("{DOWN}") }
+
+# Wait for 0.5 seconds
+Start-Sleep -Milliseconds 500
+
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+
+1..3 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait("{DOWN}") }
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+# Wait for 1 second
+Start-Sleep -Seconds 1
+
+# Tab 2 times
+1..2 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait("{TAB}") }
+
+# Press Up arrow key
+[System.Windows.Forms.SendKeys]::SendWait("{UP}")
+
+# Press Enter
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+# Press Alt + F4 to close Local Group Policy Editor
+[System.Windows.Forms.SendKeys]::SendWait("%{F4}")
+
+# Change wallpaper
+# Specify the path to the default Windows 10 wallpaper
+$wallpaperPath = "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+
+
+# Set the wallpaper using the SystemParametersInfo function
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Wallpaper {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+"@
+
+[Wallpaper]::SystemParametersInfo(0x0014, 0, $wallpaperPath, 0x01)
+
+# Restart explorer
+Stop-Process -Name explorer -Force
+Start-Process explorer
+
+
+
 # Run ngrok with specified parameters
 Start-Process -FilePath "C:\ngrok\ngrok.exe" -ArgumentList "tcp", "--region", "ap", "3389"
 
