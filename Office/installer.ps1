857b6569ff4b13b05507c7e8ec0bebafb468320e
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-16 04:47:46 +0630

    Update installer.ps1

--- Office/installer.ps1
+++ Office/installer.ps1
@@ -1,12 +1,24 @@
 # Hide progress bar
 $ProgressPreference = 'SilentlyContinue'
 
+Write-Host "Downloading offline installer"
 # Download setup
-Invoke-WebRequest -Uri "https://officecdn.microsoft.com/pr/wsus/setup.exe" -outfile "c:\setup.exe"
+Invoke-WebRequest -Uri "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/O365ProPlusRetail.img" -outfile "C:\Users\Administrator\Downloads\O365ProPlusRetail.img"
 
-# Download config
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UwU990099/myfiles/main/Configuration.xml" -outfile "C:\Configuration.xml"
+# Provide the path to your image file
+$imagePath = "C:\Users\Administrator\Downloads\O365ProPlusRetail.img"
 
-# Install
-Set-Location -Path 'C:\'
-Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'Configuration.xml' -Wait
+# Mount the image
+Mount-DiskImage -ImagePath $imagePath
+
+# Get the mounted disk
+$mountedDisk = Get-DiskImage -ImagePath $imagePath | Get-Volume
+
+# Specify the path to setup.exe within the mounted drive
+$setupPath = "$($mountedDisk.DriveLetter):\Path\To\setup.exe"
+
+# Run setup.exe
+Start-Process -FilePath $setupPath -Wait
+
+# Dismount the image when done
+Dismount-DiskImage -ImagePath $imagePath
