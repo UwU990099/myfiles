de5e4f7c587f2944dfff5e11d6a502ad28b52c45
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-21 17:36:20 +0630

    Update apps_install.ps1

--- apps_install.ps1
+++ apps_install.ps1
@@ -2,6 +2,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install internet-download-manager -y
+Start-Process "C:\Program Files (x86)\Internet Download Manager\IDMan.exe"
 choco install winrar -y
 choco install telegram -y
 
