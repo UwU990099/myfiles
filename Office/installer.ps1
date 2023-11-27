4c9f9dfdc3d4fefdad1cfbb315ce0acd2a1db9d7
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-11-27 07:41:11 +0630

    Update installer.ps1

--- Office/installer.ps1
+++ Office/installer.ps1
@@ -2,30 +2,35 @@
 $ProgressPreference = 'SilentlyContinue'
 
 Write-Host "Downloading offline installer"
-# Download setup
-Invoke-WebRequest -Uri "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/O365ProPlusRetail.img" -outfile "C:\Users\Administrator\Downloads\O365ProPlusRetail.img"
+# Download setup with -OutFile and different Proxy settings
+Invoke-WebRequest -Uri "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/O365ProPlusRetail.img" -OutFile "C:\Users\Administrator\Downloads\O365ProPlusRetail.img" -Proxy ([System.Net.WebRequest]::DefaultWebProxy) -UseBasicParsing
 
-# Provide the path to your image file
+# Check if the file exists
 $imagePath = "C:\Users\Administrator\Downloads\O365ProPlusRetail.img"
+if (Test-Path $imagePath) {
+    Write-Host "Download successful. Proceeding with the installation."
 
-# Mount the image
-Mount-DiskImage -ImagePath $imagePath
+    # Mount the image
+    Mount-DiskImage -ImagePath $imagePath
 
-# Get the mounted disk
-$mountedDisk = Get-DiskImage -ImagePath $imagePath | Get-Volume
+    # Get the mounted disk
+    $mountedDisk = Get-DiskImage -ImagePath $imagePath | Get-Volume
 
-# Change the PowerShell directory to the mounted drive
-Set-Location -Path "$($mountedDisk.DriveLetter):"
+    # Change the PowerShell directory to the mounted drive
+    Set-Location -Path "$($mountedDisk.DriveLetter):"
 
-# Run setup.exe using Start-Process
-Start-Process -FilePath "setup.exe"
+    # Run setup.exe using Start-Process
+    Start-Process -FilePath "setup.exe"
 
-Write-Host "Press anything after the installation is finished..."
-Read-Host
+    Write-Host "Press anything after the installation is finished..."
+    Read-Host
 
-# Dismount the image when done
-Dismount-DiskImage -ImagePath $imagePath
+    # Dismount the image when done
+    Dismount-DiskImage -ImagePath $imagePath
 
-Remove-Item -Path $imagePath -Force
+    Remove-Item -Path $imagePath -Force
 
-irm https://massgrave.dev/get | iex
+    irm https://massgrave.dev/get | iex
+} else {
+    Write-Host "Failed to download the file. Please check the URL and try again."
+}
