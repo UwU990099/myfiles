b12ee71aa8258c49495fbf099833bb211f1a29d2
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-12-31 20:38:40 +0630

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -137,13 +137,10 @@ $shortcut.IconLocation = "$chromePath,0"
 $shortcut.Save()
 
 # Remove folders and files
-Remove-Item -Path "C:\gp" -Recurse -Force -Wait
-Remove-Item -Path "C:\LGPO" -Recurse -Force -Wait
-Remove-Item -Path "C:\Ruby*" -Recurse -Force -Wait
-Remove-Item -Path "C:\gp.zip" -Force -Wait
-Remove-Item -Path "C:\ngrok.zip" -Force -Wait
-
-# Refresh the desktop to apply changes
-Stop-Process -Name explorer -Force
+Remove-Item -Path "C:\gp" -Recurse -Force 
+Remove-Item -Path "C:\LGPO" -Recurse -Force 
+Remove-Item -Path "C:\Ruby*" -Recurse -Force 
+Remove-Item -Path "C:\gp.zip" -Force 
+Remove-Item -Path "C:\ngrok.zip" -Force 
 
 Start-Sleep -Seconds 1
