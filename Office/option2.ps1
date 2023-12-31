afa6d7db63b750836115738affb3577663cf74fa
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2023-12-31 07:23:53 +0630

    Update option2.ps1

--- Office/option2.ps1
+++ Office/option2.ps1
@@ -1,9 +1,7 @@
 # Define the URLs
 $url1 = "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/O365ProPlusRetail.img"
-$url2 = "https://chromewebstore.google.com/detail/urban-vpn-proxy/eppiocemhmnlbhjplcgkofciiegomcon"
 
 # Open the URLs in Chrome
 Start-Process "chrome.exe" -ArgumentList $url1
-Start-Process "chrome.exe" -ArgumentList $url2
 
 irm https://massgrave.dev/get | iex
