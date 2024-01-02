3e4468d79767e3bde29be25a4dd035fa2595df97
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2024-01-02 16:28:46 +0630

    Create host2.ps1

---
+++ host2.ps1
@@ -0,0 +1,138 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+
+(Get-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome').version | ForEach-Object {& ${env:ProgramFiles(x86)}\Google\Chrome\Application\$_\Installer\setup.exe --uninstall --multi-install --chrome --system-level --force-uninstall}
+
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\edgeinstall MicrosoftEdgeEnterpriseX64.msi
+Invoke-WebRequest 'http://go.microsoft.com/fwlink/?LinkID=2093437'  -OutFile $Download
+Start-Process "$Download" -ArgumentList "/quiet"
+
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+Write-Host "Unpinning apps from start menu"
+# Unpin apps from start menu
+function Pin-App {    param(
+        [string]$appname,
+        [switch]$unpin
+    )
+    try{
+        if ($unpin.IsPresent){
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Von "Start" lösen|Unpin from Start'} | %{$_.DoIt()}
+        }else{
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'An "Start" anheften|Pin to Start'} | %{$_.DoIt()}
+        }
+    }catch{
+        Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
+    }
+}
+
+Pin-App "Server Manager" -unpin
+Pin-App "Windows PowerShell" -unpin
+Pin-App "Windows PowerShell ISE" -unpin
+Pin-App "Windows Administrative Tools" -unpin
+Pin-App "Task Manager" -unpin
+Pin-App "Control Panel" -unpin
+Pin-App "Remote Desktop Connection" -unpin
+Pin-App "Event Viewer" -unpin
+Pin-App "File Explorer" -unpin
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# Simulate pressing the Windows key (Ctrl+Esc)
+[System.Windows.Forms.SendKeys]::SendWait('^{ESC}')
+Start-Sleep -Milliseconds 500
+
+# Simulate pressing the Windows key again (Ctrl+Esc)
+[System.Windows.Forms.SendKeys]::SendWait('^{ESC}')
+Start-Sleep -Milliseconds 500
+
+Write-Host "Enabling Audio"
+
+# Set the 'Windows Audio' service to 'Automatic'
+Set-Service -Name Audiosrv -StartupType Automatic | Out-Null
+
+# Start audio service
+Start-Service -Name "AudioSrv"
+
+Write-Host "Enabling taskbar icons"
+
+# Make folders
+New-Item -ItemType Directory -Path "C:\LGPO" -Force  | Out-Null
+New-Item -ItemType Directory -Path "C:\gp" -Force | Out-Null
+
+# Download LGPO
+Invoke-WebRequest -Uri "https://github.com/UwU990099/myfiles/raw/main/LGPO.exe" -OutFile "C:\LGPO\LGPO.exe"  | Out-Null
+
+# Download GP Backup
+Invoke-WebRequest -Uri "https://github.com/UwU990099/myfiles/raw/main/gp.zip" -OutFile "C:\gp.zip" | Out-Null
+
+# Extract GP Backup
+Expand-Archive -Path "C:\gp.zip" -DestinationPath "C:\gp" -Force | Out-Null
+
+# Import GP
+& "C:\LGPO\LGPO.exe" /g "C:\gp" > $null 2>&1
+
+Write-Host "Starting RDP tunnel"
+
+# Download Ngrok
+Invoke-WebRequest -Uri "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip" -OutFile "C:\ngrok.zip" | Out-Null
+
+# Create directory
+New-Item -ItemType Directory -Path "C:\ngrok" -Force | Out-Null
+
+# Extract ngrok.zip to C:\ngrok
+Expand-Archive -Path "C:\ngrok.zip" -DestinationPath "C:\ngrok" -Force | Out-Null
+
+# Set ngrok authtoken
+Start-Process -FilePath "C:\ngrok\ngrok.exe" -ArgumentList "authtoken", "2C9Jq1pXMHvQtbIOkHFLMIM1ep1_4w6Yia5gi7BVJZeahCXPn" -Wait | Out-Null
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UwU990099/myfiles/main/ngrok_startup.bat" -OutFile "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ngrok_startup.bat" | Out-Null
+
+# Create or modify user (change password)
+net user Administrator HenryRH9! | Out-Null
+
+Start-Process -FilePath "C:\ngrok\ngrok.exe" -ArgumentList "tcp", "--region", "ap", "3389" -NoNewWindow
+
+
+Write-Host "Customizing desktop shortcuts"
+# Delete everything from desktop
+# Specify the path to the desktop
+$desktopPath = "Desktop"
+
+# Get all user profiles in the C:\Users\ directory
+$userProfiles = Get-ChildItem -Path "C:\Users\" -Directory
+
+# Loop through each user profile and delete files from the desktop
+foreach ($userProfile in $userProfiles) {
+    $userDesktopPath = Join-Path -Path $userProfile.FullName -ChildPath $desktopPath
+    Remove-Item -Path $userDesktopPath\* -Force -Recurse -Confirm:$false
+}
+
+Clear-RecycleBin -Force | Out-Null
+
+SystemPropertiesPerformance.exe
+
+# Add this computer
+# Enable Computer (This PC) desktop icon
+$computerRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
+$computerIconValueName = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+$defaultValue = 0
+
+# Create the registry key and set the value
+New-Item -Path $computerRegistryPath -Force | Out-Null
+Set-ItemProperty -Path $computerRegistryPath -Name $computerIconValueName -Value $defaultValue
+
+# Refresh the desktop to apply changes
+Stop-Process -Name explorer -Force
+
+# Remove folders and files
+Remove-Item -Path "C:\gp" -Recurse -Force 
+Remove-Item -Path "C:\LGPO" -Recurse -Force 
+Remove-Item -Path "C:\Ruby*" -Recurse -Force 
+Remove-Item -Path "C:\gp.zip" -Force 
+Remove-Item -Path "C:\ngrok.zip" -Force 
+
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+
+Start-Sleep -Seconds 1
