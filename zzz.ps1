b433befe6663f295e49d26a535f362799185d4f6
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2024-01-03 19:46:19 +0630

    Update zzz.ps1

--- zzz.ps1
+++ zzz.ps1
@@ -1,8 +1,9 @@
-# Registry path and entry name
+# Deleting Edge icon
+rm "C:\Users\Administrator\Desktop\Microsoft Edge.lnk"
+
+# Enable task view button
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView\AllUpView"
 $registryEntryName = "Enabled"
-
-# Check if the registry entry exists before attempting to remove it
 if (Test-Path -Path $registryPath) {
     $registryValue = Get-ItemProperty -Path $registryPath -Name $registryEntryName -ErrorAction SilentlyContinue
     if ($registryValue -ne $null) {
@@ -12,21 +13,17 @@ if (Test-Path -Path $registryPath) {
     }
 } else {
 }
-
-# Add registry entry
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowTaskViewButton -Type DWord -Value 1
-
-# Restart Explorer
 Stop-Process -Name explorer -Force
 
-# Define the URLs
-$url1 = "https://chromewebstore.google.com/detail/auto-refresh-plus-page-mo/hgeljhfekpckiiplhkigfehkdpldcggm?pli=1"
-$url2 = "https://chromewebstore.google.com/detail/urban-vpn-proxy/eppiocemhmnlbhjplcgkofciiegomcon"
+# Open the URLs
+$url1 = "https://microsoftedge.microsoft.com/addons/detail/urban-vpn-proxy/nimlmejbmnecnaghgmbahmbaddhjbecg"
+$url2 = "https://microsoftedge.microsoft.com/addons/detail/auto-refresh-plus-page-/oiecibnnapjeijccpmhonpooadjpcmnk"
 $url3 = "https://www.coursera.org/login"
 $url4 = "https://justpaste.it/hka"
 
 # Open the URLs in Chrome
-Start-Process "chrome.exe" -ArgumentList $url1
-Start-Process "chrome.exe" -ArgumentList $url2
-Start-Process "chrome.exe" -ArgumentList $url3
-Start-Process "chrome.exe" -ArgumentList $url4
+Start-Process "msedge.exe" -ArgumentList $url1
+Start-Process "msedge.exe" -ArgumentList $url2
+Start-Process "msedge.exe" -ArgumentList $url3
+Start-Process "msedge.exe" -ArgumentList $url4
