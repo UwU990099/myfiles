376f5f25c7fa9dc143beb8ca966427ce9b1f318a
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2024-01-04 08:07:50 +0630

    Update host.ps1

--- 2h/host.ps1
+++ 2h/host.ps1
@@ -109,3 +109,5 @@ $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut("C:\Users\localadmin\Desktop\LosslessCut.lnk")
 $shortcut.TargetPath = "C:\LosslessCut\LosslessCut.exe"
 $shortcut.Save()
+
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
